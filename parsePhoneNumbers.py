c2c152420ffdc0660272b73488625f1cb84bc2e8
muokicaleb <muokicaleb@gmail.com>
2018-08-14 09:08:54 +0300

    print numbers

--- parsePhoneNumbers.py
+++ parsePhoneNumbers.py
@@ -1,10 +1,10 @@
 import re
 
-numerPattern = re.compile(r"\d(3).\d(3).\d(4)")
+numberPattern = re.compile(r"\d{3}.\d{3}.\d{4}")
 with open('data.txt', 'r') as file:
     contents = file.read()
 
-    numberMatches = numerPattern.finditer(contents)
+    numberMatches = numberPattern.findall(contents)
 
     for number in numberMatches:
         print(number)
