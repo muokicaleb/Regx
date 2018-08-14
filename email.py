bd523f7ba8201c1fcb78c51104be41edec6f5ca4
muokicaleb <muokicaleb@gmail.com>
2018-08-14 09:08:36 +0300

    print emails

--- email.py
+++ email.py
@@ -4,7 +4,7 @@ emailPattern = re.compile(r"[a-zA-Z0-9.+-_]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
 with open('data.txt', 'r') as file:
     contents = file.read()
 
-    emailMatches = emailPattern.finditer(contents)
+    emailMatches = emailPattern.findall(contents)
 
     for email in emailMatches:
         print(email)
